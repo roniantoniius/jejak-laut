610e3a2729889dfbca53bcf3b2e1f90884503b68
roniantonius <roniantoniuss@upi.edu>
2024-12-26 23:31:19 +0700

    Getting an response error Invalid Call

--- mobile/jelamobile/JejakLaut/components/GuideStep.tsx
+++ mobile/jelamobile/JejakLaut/components/GuideStep.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, ImageSourcePropType } from 'react-native';
 import { Asset } from 'expo-asset';
 
 type GuideStepProps = {
@@ -10,12 +10,17 @@ type GuideStepProps = {
 
 export const GuideStep: React.FC<GuideStepProps> = ({ stepNumber, imageSrc, description }) => {
   // Use expo-asset to load the image dynamically
-  const image = Asset.fromModule(require(`../assets/images/${imageSrc}`));
+  const imageAsset = Asset.fromModule(require(`../assets/images/${imageSrc}`));
+  
+  // Ensure we have an image source that matches ImageSourcePropType
+  const source: ImageSourcePropType = imageAsset.localUri 
+    ? { uri: imageAsset.localUri } 
+    : require(`../assets/images/${imageSrc}`);
 
   return (
     <View style={styles.stepContainer}>
       <Image 
-        source={image}
+        source={source}
         style={styles.image}
       />
       <Text style={styles.text}>{`${stepNumber}. ${description}`}</Text>
