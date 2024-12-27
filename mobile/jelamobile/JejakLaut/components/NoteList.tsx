8c447cf346405b72f7faca74f9d7789185829932
roniantonius <roniantoniuss@upi.edu>
2024-12-27 13:45:32 +0700

    ADding icon into search filter and were got an conflitct that after typing 1 word the keyboard closed

--- mobile/jelamobile/JejakLaut/components/NoteList.tsx
+++ mobile/jelamobile/JejakLaut/components/NoteList.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useMemo } from 'react';
-import { FlatList, View, StyleSheet, TextInput, Text } from 'react-native';
+import { FlatList, View, StyleSheet, TextInput } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import { NoteCard } from './NoteCard';
 import { NoteData, Tag } from './types';
 
@@ -12,41 +13,65 @@ export function NoteList({ tags, notes }: NoteListProps) {
   const [searchTitle, setSearchTitle] = useState('');
   const [searchTag, setSearchTag] = useState('');
 
-  // Function to handle note press
   const handlePressNote = (id: string) => {
     console.log(`Note pressed: ${id}`);
   };
 
-  // Filter notes based on search criteria
   const filteredNotes = useMemo(() => {
-    return notes.filter((note) => {
-      const titleMatch = note.title.toLowerCase().includes(searchTitle.toLowerCase());
-      const tagMatch = note.tags.some(tag => tag.label.toLowerCase().includes(searchTag.toLowerCase()));
-      return titleMatch && (searchTag === '' || tagMatch);
-    }).sort((a, b) => new Date(b.lastModified).getTime() - new Date(a.lastModified).getTime());
+    return notes
+      .filter((note) => {
+        const titleMatch = note.title
+          .toLowerCase()
+          .includes(searchTitle.toLowerCase());
+        const tagMatch = note.tags.some((tag) =>
+          tag.label.toLowerCase().includes(searchTag.toLowerCase())
+        );
+        return titleMatch && (searchTag === '' || tagMatch);
+      })
+      .sort(
+        (a, b) =>
+          new Date(b.lastModified).getTime() -
+          new Date(a.lastModified).getTime()
+      );
   }, [notes, searchTitle, searchTag]);
+
+  const renderHeader = () => (
+    <View style={styles.searchContainer}>
+      <View style={styles.iconInputContainer}>
+        <Ionicons name="search" size={20} color="#052844" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          placeholder="Cari Judul..."
+          onChangeText={setSearchTitle}
+          value={searchTitle}
+          placeholderTextColor="#052844"
+        />
+      </View>
+      <View style={styles.iconInputContainer}>
+        <Ionicons
+          name="pricetag-outline"
+          size={20}
+          color="#052844"
+          style={styles.icon}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Cari Tag..."
+          onChangeText={setSearchTag}
+          value={searchTag}
+          placeholderTextColor="#052844"
+        />
+      </View>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
-      <View style={styles.searchContainer}>
-        <TextInput 
-          style={styles.input} 
-          placeholder="Cari Judul..." 
-          onChangeText={setSearchTitle} 
-          value={searchTitle}
-          placeholderTextColor={'#052844'}
-        />
-        <TextInput 
-          style={styles.input} 
-          placeholder="Cari Tag..." 
-          onChangeText={setSearchTag} 
-          value={searchTag}
-          placeholderTextColor={'#052844'}
-        />
-      </View>
       <FlatList
         data={filteredNotes}
         keyExtractor={(item) => item.id}
+        style={styles.lisTo}
+        ListHeaderComponent={renderHeader}
         renderItem={({ item }) => (
           <NoteCard
             id={item.id}
@@ -70,17 +95,28 @@ const styles = StyleSheet.create({
     marginBottom: 63,
   },
   searchContainer: {
-    padding: 10,
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  iconInputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 12,
+    borderWidth: 1,
+    borderColor: '#e4e4e4',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+  },
+  icon: {
+    marginRight: 8,
   },
   input: {
     flex: 1,
     height: 40,
-    margin: 12,
-    borderWidth: 1,
+    color: '#052844',
+  },
+  lisTo: {
     padding: 10,
-    borderRadius: 8,
-    borderColor: '#e4e4e4',
   },
 });
